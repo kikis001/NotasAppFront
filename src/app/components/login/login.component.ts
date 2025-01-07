858899966a6497019fa86f05e6268c70c60e7d98
Luis Enrique <kikisxx.150@gmail.com>
2025-01-07 02:13:04 -0600

    formularios reactivos y alertas

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,16 +1,41 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { MaterialModule } from '../../material/material/material.module';
 import { Router } from '@angular/router';
+import {
+  ReactiveFormsModule,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [MaterialModule],
+  imports: [MaterialModule, ReactiveFormsModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  constructor(private route: Router) {}
+  loginForm = new FormGroup({
+    email: new FormControl<string>('', [Validators.required, Validators.email]),
+    password: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+
+  isButtonDisabled = true;
+
+  constructor(private route: Router) {
+    this.loginForm.statusChanges.subscribe((status) => {
+      this.isButtonDisabled = !this.loginForm.valid;
+    });
+  }
 
   hide = signal(true);
   clickEvent(event: MouseEvent) {
@@ -21,5 +46,14 @@ export class LoginComponent {
 
   register() {
     this.route.navigate(['/register']);
+  }
+
+  // solo faltaría implementar la lógica de login
+  login(e: Event) {
+    if(!this.loginForm.valid) {
+      return;
+    }
+    this.isButtonDisabled = true;
+    console.log(this.loginForm.value);
   }
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,16 +1,30 @@
 import { Component, signal } from '@angular/core';
 import { MaterialModule } from '../../material/material/material.module';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [MaterialModule],
+  imports: [MaterialModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  registerForm = new FormGroup({
+    name: new FormControl<string>('', [Validators.required]),
+    email: new FormControl<string>('', [Validators.required, Validators.email]),
+    password: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+  isButtonDisabled = true;
 
-  constructor(private route: Router) {}
+  constructor(private route: Router) {
+    this.registerForm.statusChanges.subscribe((status) => {
+      this.isButtonDisabled = !this.registerForm.valid;
+    });
+  }
 
   hide = signal(true);
   clickEvent(event: MouseEvent) {
@@ -19,7 +33,15 @@ export class RegisterComponent {
     event.preventDefault();
   }
 
-  login() {
+  goLogin() {
     this.route.navigate(['/login']);
+  }
+
+  register(e: Event) {
+    if(!this.registerForm.valid) {
+      return;
+    }
+    this.isButtonDisabled = true;
+    console.log(this.registerForm.value);
   }
 }
